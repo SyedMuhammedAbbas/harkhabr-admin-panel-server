f89a5c0fd16f9b42f61783ecc9b9da48510b959f
SyedMuhammedAbbas <syedmuhammedabbas@gmail.com>
2022-09-12 22:52:21 +0500

    Updated

--- routes/postnews.js
+++ routes/postnews.js
@@ -19,21 +19,18 @@ router.put("/:id", async (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   try {
     const post = await Post.findById(req.params.id);
-    if (post.username === req.body.username) {
-      try {
-        const updatedPost = await Post.findByIdAndUpdate(
-          req.params.id,
-          {
-            $set: req.body,
-          },
-          { new: true }
-        );
-        res.status(200).json(updatedPost);
-      } catch (err) {
-        res.status(500).json(err);
-      }
-    } else {
-      res.status(401).json("You can update only your post!");
+
+    try {
+      const updatedPost = await Post.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: req.body,
+        },
+        { new: true }
+      );
+      res.status(200).json(updatedPost);
+    } catch (err) {
+      res.status(500).json(err);
     }
   } catch (err) {
     res.status(500).json(err);
@@ -45,15 +42,12 @@ router.delete("/:id", async (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   try {
     const post = await Post.findById(req.params.id);
-    if (post.username === req.body.username) {
-      try {
-        await post.delete();
-        res.status(200).json("Post has been deleted...");
-      } catch (err) {
-        res.status(500).json(err);
-      }
-    } else {
-      res.status(401).json("You can delete only your post!");
+
+    try {
+      await post.delete();
+      res.status(200).json("Post has been deleted...");
+    } catch (err) {
+      res.status(500).json(err);
     }
   } catch (err) {
     res.status(500).json(err);
@@ -64,7 +58,6 @@ router.delete("/:id", async (req, res) => {
 router.get("/:id", async (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   try {
-  
     const post = await Post.findById(req.params.id);
     res.status(200).json(post);
   } catch (err) {
@@ -78,11 +71,10 @@ router.get("/", async (req, res) => {
 
   try {
     let posts;
-    
-      posts = await Post.find();
-    
+
+    posts = await Post.find();
+
     res.status(200).json(posts);
-   
   } catch (err) {
     res.status(500).json(err);
   }
